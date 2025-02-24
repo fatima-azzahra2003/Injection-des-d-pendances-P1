238d0025b2b1fd80dac77f3a780461147dddf8db
fatima-azzahra2003 <fatimaazzahrahamdaoui3@gmail.com>
2025-02-24 14:45:23 +0000

    4eme commit

--- src/main/java/com/example/Main/App.java
+++ src/main/java/com/example/Main/App.java
@@ -3,15 +3,19 @@ package com.example.Main;
 import com.example.Metier.IMetier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 public class App {
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 
-        IMetier metier = (IMetier) context.getBean("metier");
+       // IMetier metier = (IMetier) context.getBean("metier");
+        // Charger le contexte Spring avec les annotations
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.example");
 
+        // Obtenir le bean Metier
+        IMetier metier = context.getBean(IMetier.class);
 
         System.out.println(metier.calcul());
     }
